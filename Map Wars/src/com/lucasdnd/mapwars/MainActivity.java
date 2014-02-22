c35badcd6a98d0a1b33c2579d3fedbf35229c569
Lucas <lucasdnd@hotmail.com>
2014-02-22 00:05:14 -0300

    Added My Location

--- Map Wars/src/com/lucasdnd/mapwars/MainActivity.java
+++ Map Wars/src/com/lucasdnd/mapwars/MainActivity.java
@@ -1,15 +1,26 @@
 package com.lucasdnd.mapwars;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Setup the Map
+		this.setupMap();
+		
+		map.setMyLocationEnabled(true);
 	}
 
 	@Override
@@ -17,6 +28,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void setupMap() {
+		
+		// Check if we already have a Map
+		if(map == null) {
+			
+			// Get the Map View
+			map = ((MapFragment) getFragmentManager().findFragmentById(R.id.mainActivity_map)).getMap();
+			
+			// Check if we're good to go
+			if(map == null) {
+				System.out.println("Could not start the Map");
+			}
+		}
 	}
 
 }
