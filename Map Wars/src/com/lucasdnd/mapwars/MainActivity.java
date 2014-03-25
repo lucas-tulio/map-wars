52d08237014bcedd73ead9ba72ad8c9a23a9d569
Lucas Tulio <tulio@commit.to>
2014-03-25 12:52:02 -0300

    Generating points in a radius around the player

--- Map Wars/src/com/lucasdnd/mapwars/MainActivity.java
+++ Map Wars/src/com/lucasdnd/mapwars/MainActivity.java
@@ -355,7 +355,7 @@ public class MainActivity extends Activity implements OnCameraChangeListener, Lo
 		for(int i = 0; i < numTargets; i++) {
 			
 			// Create the Targets
-			MapCircle target = new MapCircle(LocationRandomizer.getRandomLatLng(userLatLng, 0.02, 0.01), 150.0, Color.argb(255, 0, 128, 255));
+			MapCircle target = new MapCircle(LocationRandomizer.getRandomLatLng(userLatLng, 5000.0, 1000.0), 150.0, Color.argb(255, 0, 128, 255));
 			target.setCircle(map.addCircle(target.getCircleOptions()));
 			targets.add(target);
 		}
--- Map Wars/src/com/lucasdnd/mapwars/game/LocationRandomizer.java
+++ Map Wars/src/com/lucasdnd/mapwars/game/LocationRandomizer.java
@@ -3,38 +3,23 @@ package com.lucasdnd.mapwars.game;
 import java.util.Random;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.lucasdnd.mapwars.maps.GeometryUtil;
 
 public class LocationRandomizer {
 	
-	public static LatLng getRandomLatLng(LatLng currentLocation, double random, double min) {
+	public static LatLng getRandomLatLng(LatLng currentLocation, double min, double max) {
 		
 		Random r = new Random();
 		
-		// Positive or negative?
-		boolean negativeLat = r.nextBoolean();
-		boolean negativeLng = r.nextBoolean();
+		// Generate a random bearing
+		double bearing = r.nextDouble() * 360;
 		
-		// Random Lat, Lng
-		double randomLat, randomLng;
-		randomLat = randomLng = random;
+		// How far?
+		double distance = (r.nextDouble() * (max - min)) + min;
 		
-		// Min Lat, Lng
-		double minLat, minLng;
-		minLat = minLng = min;
+		// Where would that be?
+		LatLng resultingLatLng = GeometryUtil.getLatLngAwayFromSource(currentLocation, distance, bearing);
 		
-		if(negativeLat) {
-			randomLat *= -1;
-			minLat *= -1;
-		}
-		if(negativeLng) {
-			randomLng *= -1;
-			minLng *= -1;
-		}
-		
-		// Get the randoms!
-		double lat = r.nextDouble() * randomLat + minLat;
-		double lng = r.nextDouble() * randomLng + minLng;
-					
-		return new LatLng(currentLocation.latitude + lat, currentLocation.longitude + lng);
+		return resultingLatLng;
 	}
 }
